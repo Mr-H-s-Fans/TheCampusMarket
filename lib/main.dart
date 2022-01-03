d798a779e3f0eb3111db71449f9b4d591ff1ad9c
ken-interested <1607588332@qq.com>
2022-01-03 20:32:31 +0800

    search change one

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'Search.dart';
 import 'package:hello/app.dart';
 
 void main() {
-  runApp(const CampusMarket());
+  runApp( const CampusMarket());
 }
+
+
+
--- lib/search.dart
+++ lib/search.dart
@@ -8,25 +8,73 @@ class Search extends StatefulWidget {
 }
 
 class SearchState extends State<Search> {
+  final TextEditingController _controller = TextEditingController();
+  late String tip;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
+          backgroundColor: Colors.black,
           title: const Text("搜索"),
           centerTitle: true,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text("Search"),
-            ],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        controller: _controller,
+                        autofocus: false,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.search),
+                          hintText: "输入要检索的内容",
+                        ),
+                      ),
+                      flex: 7,
+                    ),
+                    Expanded(
+                      child: TextButton(
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (ctx) {
+                                  if (_controller.text.isEmpty) {
+                                    tip = "检索内容不能为空";
+                                  } else {
+                                    tip = "正在搜索：${_controller.text}";
+                                  }
+                                  return SimpleDialog(
+                                    title: Text("提示"),
+                                    children: <Widget>[
+                                      ListTile(
+                                        title: Center(
+                                          child: Text(tip),
+                                        ),
+                                      ),
+                                    ],
+                                  );
+                                });
+                          },
+                          child: Text("搜索")),
+                      flex: 1,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
-
 }
